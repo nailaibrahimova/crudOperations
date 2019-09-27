d18b305646fea39cc4363c282c4445cb4e44229a
nailaibrahimova <48138783+nailaibrahimova@users.noreply.github.com>
2019-09-27 12:30:01 +0400

    update

--- src/main/java/az/pashabank/learning_sessions/crud_operations_with_database/repository/CustomerRepository.java
+++ src/main/java/az/pashabank/learning_sessions/crud_operations_with_database/repository/CustomerRepository.java
@@ -1,7 +1,6 @@
 package az.pashabank.learning_sessions.crud_operations_with_database.repository;
 
 import az.pashabank.learning_sessions.crud_operations_with_database.dao.CustomerEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
 public interface CustomerRepository extends CrudRepository<CustomerEntity, Long> {
